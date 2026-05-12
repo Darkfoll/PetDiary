740d345e0e55a07d00a8174a18e67130d80524b0
Darkfoll <vanar8677@gmail.com>
2026-05-12 13:56:15 +0300

    Создан лобби дневнмиков и их создание

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     // Room Database
     implementation("androidx.room:room-runtime:2.6.0")
     implementation("androidx.room:room-ktx:2.6.0")
-
+    implementation("com.google.android.material:material:1.11.0")
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
